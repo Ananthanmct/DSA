31a850114fc5cf9ec6e55d496b8da6fbb73c8bdf
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2024-12-21 16:37:39 +0530

    Create DeleteNode.java

---
+++ Offline/DeleteNode.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode next =  node.next;
+        int temp = node.val;
+        node.val = next.val;
+        next.val = temp;
+        node.next = node.next.next;
+    }
+}
