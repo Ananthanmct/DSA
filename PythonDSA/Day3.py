795d5faf23a52c21e8a65110b8e2fce744513ea2
Somendra Tiwari <somadritiwari@Somadris-MacBook-Air.local>
2025-01-03 15:27:41 +0530

    Day3 class code added

---
+++ PythonDSA/Day3.py
@@ -0,0 +1,37 @@
+# Sum of n natural numbers 
+
+# sum = 0
+# num = 1
+# n = int(input("Enter value for n: "))
+
+# while num <= n:
+#     sum = sum + num
+#     num = num + 1
+
+
+# print(sum)
+
+
+# For Loop approach 
+
+# n = int(input("Enter value for n: "))
+# sum = 0
+
+# for i in range(1, n + 1):
+#     sum = sum + i
+
+# print(sum)
+
+# Print all even numbers from 1 to n
+
+# n = int(input("Eneter value for n: ")) # 6
+# num = 1 # -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+
+# while num <= n: # 7 <= 6
+#     if num%2 == 0: # 6%2 == 0
+#         print(num)
+#     num = num + 1 
+
+
+
+    
