d9e26ae1bf924e34fe5018944d088c6fb2ff0aaa
Muskan-apnaworld <muskan.maths2021@gmail.com>
2025-01-28 07:30:53 +0530

    Add

---
+++ PythonDSA/Loops.py/PrintDigits.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the value:"))
+
+for digit in str(n):   #Here, digit is variable so it can be anything
+    print(digit)
+
+#-------------------------------------------------------------******------------------------------------------------------#
+
+n = str(input("Enter the word:"))
+
+for name in n:
+    print(name)
