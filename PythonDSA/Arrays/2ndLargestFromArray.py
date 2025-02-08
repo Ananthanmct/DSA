ea5800f525a4850d08ab0a4b806472cf5ef4695e
Muskan-apnaworld <muskan.maths2021@gmail.com>
2025-02-08 20:23:52 +0530

    2nd Largest Number of array

---
+++ PythonDSA/Arrays/2ndLargestFromArray.py
@@ -0,0 +1,34 @@
+def second_largest_element(arr):
+    Smax = -10**9
+    max = -10**9 #7
+    i = 0
+    while i < len(arr):
+        if arr[i] > max:
+            max = arr[i]
+        i += 1
+
+    i = 0 
+    while i < len(arr):
+        if arr[i] < max and arr[i] > Smax:
+            Smax = arr[i]
+        i +=1
+    return Smax    
+ 
+def second_approch(arr):
+    max = Smax = -10**9
+    i = 0
+    while i < len(arr):
+        if arr[i] > max:
+            Smax = max
+            max = arr[i]
+
+        elif arr[i] > Smax:
+            Smax = arr[i]
+    
+        i += 1
+
+    return Smax
+
+arr = [5,6,7,11,10,3]
+result = second_approch(arr)
+print(result)
