64f149359fbde4e9fb59bbae0a5145199a43f25f
Somendrat22 <somendrat22@gmail.com>
2025-02-11 22:54:01 +0530

    Created db-api model layer for zepto

---
+++ spring-feb-2025/zepto-clone/db-api/src/main/java/com/db/zepto/db_api/model/AppOrder.java
@@ -0,0 +1,24 @@
+package com.db.zepto.db_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+public class AppOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    UUID id;
+    LocalDateTime placedTime;
+    @ManyToOne
+    AppUser customer;
+    @ManyToOne
+    AppUser deliveryPartner;
+    int totalAmount;
+    @OneToMany
+    List<Product> products;
+}
