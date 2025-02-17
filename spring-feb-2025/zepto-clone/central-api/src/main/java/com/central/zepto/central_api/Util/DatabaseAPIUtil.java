0c01d75713b65ea07b4fb1dc2ce56397b31adf4e
Somendrat22 <somendrat22@gmail.com>
2025-02-17 21:50:27 +0530

    Zepto: Generalized api calls

--- spring-feb-2025/zepto-clone/central-api/src/main/java/com/central/zepto/central_api/Util/DatabaseAPIUtil.java
+++ spring-feb-2025/zepto-clone/central-api/src/main/java/com/central/zepto/central_api/Util/DatabaseAPIUtil.java
@@ -2,51 +2,47 @@ package com.central.zepto.central_api.Util;
 
 import com.central.zepto.central_api.models.AppUser;
 import com.central.zepto.central_api.models.Product;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
+import java.util.HashMap;
 
 @Component
-public class DatabaseAPIUtil {
+public class DatabaseAPIUtil extends ApiUtilImpl {
 
-    String dbApiUrl = "/api/v1/db";
+    @Value("${db.api.url}")
+    String dbApiUrl;
+
+    @Autowired
+    ModelMapper mapper;
 
     public AppUser callCreateUserEndpoint(AppUser user){
         //creation of url
-        String url = "http://localhost:8081" + dbApiUrl + "/user/save";
-        URI finalUrl = URI.create(url);
-        //create request entity
-        RequestEntity request = RequestEntity.post(finalUrl).body(user);
-        // Create rest Template
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<AppUser> response =restTemplate.exchange(url, HttpMethod.POST, request, AppUser.class);
-        return response.getBody();
+//        String url = "http://localhost:8081" + dbApiUrl + "/user/save";
+//        URI finalUrl = URI.create(url);
+//        //create request entity
+//        RequestEntity request = RequestEntity.post(finalUrl).body(user);
+//        // Create rest Template
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<AppUser> response =restTemplate.exchange(url, HttpMethod.POST, request, AppUser.class);
+//        return response.getBody();
+
+        Object resp = makePostCall(dbApiUrl, "/user/save", new HashMap<>(), user);
+        AppUser userResp = mapper.map(resp, AppUser.class);
+        return userResp;
     }
 
     public Product callCreateProductEndPoint(Product product){
+        Object resp = makePostCall(dbApiUrl, "/product/save", new HashMap<>(), product);
+        return mapper.map(resp, Product.class);
 
-        String url = "http://localhost:8081" + dbApiUrl + "/product/save";
-        URI finalUrl = URI.create(url);
-
-        RequestEntity request = RequestEntity.post(finalUrl).body(product);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Product> response = restTemplate.exchange(finalUrl, HttpMethod.POST, request, Product.class);
-        return response.getBody();
     }
 
     public AppUser getUserByEmail(String email){
-        String url = "http://localhost:8081" + dbApiUrl + "/user/email/" + email;
-        URI finalURl = URI.create(url);
-
-        RequestEntity request = RequestEntity.get(finalURl).build();
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<AppUser> response = restTemplate.exchange(url, HttpMethod.GET, request, AppUser.class);
-        return response.getBody();
+        String endPoint =  "/user/email/" + email;
+        Object resp  = makeGetCall(dbApiUrl, endPoint, new HashMap<>());
+        return mapper.map(resp, AppUser.class);
     }
 }
