cfdeb0d5817ab49013cba0c2a3bde274ec4b793a
Somendrat22 <somendrat22@gmail.com>
2025-02-20 21:03:00 +0530

    Zepto: WareHouse Products db endpoints done

--- spring-feb-2025/zepto-clone/db-api/src/main/java/com/db/zepto/db_api/controller/WareHouseProductController.java
+++ spring-feb-2025/zepto-clone/db-api/src/main/java/com/db/zepto/db_api/controller/WareHouseProductController.java
@@ -3,10 +3,10 @@ package com.db.zepto.db_api.controller;
 import com.db.zepto.db_api.model.WareHouseProducts;
 import com.db.zepto.db_api.repository.WareHouseProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/db/warehouse/product")
@@ -19,5 +19,10 @@ public class WareHouseProductController {
     public WareHouseProducts addProduct(@RequestBody WareHouseProducts wareHouseProducts){
         wareHouseProductsRepository.save(wareHouseProducts);
         return wareHouseProducts;
+    }
+
+    @GetMapping("/{wid}")
+    public List<WareHouseProducts> getAllProductsByWid(@PathVariable UUID wid){
+        return wareHouseProductsRepository.getWareHouseProductsByWid(wid);
     }
 }
