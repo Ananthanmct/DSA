e7d6df1ad1a44756bb16aa056f7995a412dd3a9c
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-03-16 18:14:39 +0530

    Create ConstructBSTPreOrderTraversal.java

---
+++ Offline/ConstructBSTPreOrderTraversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int idx = 0;
+    public TreeNode constructTree(int [] preorder, int leftR, int rightR){
+        if(idx >= preorder.length){
+            return null;
+        }
+
+        if(preorder[idx] <= leftR ||  preorder[idx] >= rightR){
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[idx]);
+        idx++;
+        root.left = constructTree(preorder, leftR, root.val);
+        root.right = constructTree(preorder, root.val, rightR);
+        return root;
+    }
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = constructTree(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return root;
+    }
+}
