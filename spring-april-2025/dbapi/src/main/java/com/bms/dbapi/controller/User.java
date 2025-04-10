35b206b1a95395aa12e8a17a810edb760e358232
Somendrat22 <somendrat22@gmail.com>
2025-04-10 22:19:37 +0530

    Database api model creation

---
+++ spring-april-2025/dbapi/src/main/java/com/bms/dbapi/controller/User.java
@@ -0,0 +1,11 @@
+package com.bms.dbapi.controller;
+
+import com.bms.dbapi.models.AppUser;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class User {
+
+    public void createUser(@ResponseBody AppUser user){
+
+    }
+}
