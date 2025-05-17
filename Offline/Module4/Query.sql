b66ab47dc105ce9ea932c198a4e7c0b8da7251ad
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-05-17 17:59:00 +0530

    Create Query.sql

---
+++ Offline/Module4/Query.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE Persons (
+--     ID int PRIMARY KEY UNIQUE,
+--     LastName varchar(255) NOT NULL,
+--     FirstName varchar(255),
+--     Age int
+-- );
+
+
+-- CREATE TABLE Orders (
+--   oid int ,
+--   pid int,
+--   CONSTRAINT personc FOREIGN KEY (pid, )
+--     REFERENCES persons(id)
+-- );
+
+-- ALTER TABLE student
+-- alter name add unique;
+
+-- ALTER TABLE orders
+-- ALTER column pid add CONSTRAINT not null;
+
+-- drop table orders
+
+-- truncate table student
+
+
+-- INSERT INTO student (sid,  name)
+-- VALUES (4,  'Somendra');
+
+-- select * from student
