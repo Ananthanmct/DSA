4e527452d82e95643dd6394677659905515b5abe
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-05-30 01:14:52 +0530

    Update RotateArray.py

--- PythonDSA/Arrays/RotateArray.py
+++ PythonDSA/Arrays/RotateArray.py
@@ -1,28 +1,29 @@
-def reverse(arr,  st, en):
-    i = st;
-    j = en;
-    
-    while i < j:
-        temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-        i += 1
-        j -= 1
-        
-
-
-
-
-arr = [2, 3, 4, 1]
-k = -1
-
-
-
-n = len(arr)
-k= k%n
-if k < 0:
-    k = k +  n
-reverse(arr, 0, n - 1)
-reverse(arr, n - k, n - 1)
-reverse(arr, 0, n - k - 1)
-print(arr)
+class Solution:
+    def reverse(self, arr, st, en):
+        i = st
+        j = en
+        while i < j:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i += 1
+            j -= 1
+    def rotateArray(self, arr, k):
+        n = len(arr)
+        k = k%n
+        if k < 0:
+            k += n
+        self.reverse(arr, 0, len(arr) - 1)
+        self.reverse(arr, 0, k - 1)
+        self.reverse(arr, k, len(arr) - 1)
+      
+if __name__ == "__main__":
+    n = int(input())
+    allNums = input().strip()
+    arr = allNums.split(" ")
+    nums = list(map(int, arr))
+    k = int(input())
+    obj = Solution()
+    obj.rotateArray(nums, k)
+    for num in nums:
+        print(num, end = " ")
