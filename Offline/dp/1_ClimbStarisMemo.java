ccaf1358b93baab5509bb64819904c2a3719746e
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2025-06-03 22:07:48 +0530

    Create 1_ClimbStarisMemo.java

---
+++ Offline/dp/1_ClimbStarisMemo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import static java.lang.Math.ceil;
+
+
+public int ClimbingStairs(int n, int [] dp) {
+    if(n == 0){
+        return 1;
+    }
+    if(dp[n] != 0){
+        return dp[n];
+    }
+    int w1 = ClimbingStairs(n - 1, dp);
+    int w2 = ClimbingStairs(n - 2, dp);
+    dp[n] = w1 + w2;
+    return w1 + w2;
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Accio Obj = new Accio();
+        int res = ClimbingStairs(n, new int[n + 1]);
+        return res;
+    }
+}
