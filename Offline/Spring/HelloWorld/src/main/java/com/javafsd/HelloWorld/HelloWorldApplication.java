92ad9fc0b4413885c217d8f1165b4d2454f00546
Somendrat22 <somendrat22@gmail.com>
2025-06-14 16:04:24 +0530

    Springissue code pushed

--- Offline/Spring/HelloWorld/src/main/java/com/javafsd/HelloWorld/HelloWorldApplication.java
+++ Offline/Spring/HelloWorld/src/main/java/com/javafsd/HelloWorld/HelloWorldApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldApplication.class, args);
+		Human human = new Human();
+		School school = new School();
 	}
 
 }
